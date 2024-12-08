62f112da6e5691492aae0613b67a49ebdcc7a352
jong6598 <jong6598@naver.com>
2024-12-09 01:29:33 +0900

    fix: 호환성 문제로 코드 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        legacy({
-            targets: ['defaults', '> 0.2%', 'not dead', 'ie >= 11'],
-            additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        }),
-    ],
+    plugins: [react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
